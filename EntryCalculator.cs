0089a1ff3913ad3807ba3571ace773c6965c96f7
Salma Azzahra Salsabila <salma.2000@students.amikom.ac.id>
2019-11-25 23:16:08 +0700

    Delagate dan Event

---
+++ EntryCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApps
+{
+    public partial class EntryCalculator : Form
+    {
+        public delegate void OperasiEventHandler(int index, int a, int b);
+
+        public event OperasiEventHandler OnPerhitungan;
+
+        
+
+        public EntryCalculator()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("...");
+            comboBox1.Items.Add("Penjumlahan");
+            comboBox1.Items.Add("Pengurangan");
+            comboBox1.Items.Add("Perkalian");
+            comboBox1.Items.Add("Pembagian");
+            comboBox1.SelectedIndex = 0;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int NilaiA = int.Parse(textBox1.Text);
+            int NilaiB = int.Parse(textBox2.Text);
+
+            if(comboBox1.SelectedIndex == 1)
+            {
+                OnPerhitungan(1, NilaiA, NilaiB);
+            }
+            else if(comboBox1.SelectedIndex == 2)
+            {
+                OnPerhitungan(2, NilaiA, NilaiB);
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                OnPerhitungan(3, NilaiA, NilaiB);
+            }
+            else if(comboBox1.SelectedIndex == 4)
+            {
+                OnPerhitungan(4, NilaiA, NilaiB);
+            }
+            this.Close();
+        }
+    }
+}
---
+++ HasilPerhitungan.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FormHasilPerhitungan(int index, int a, int b)
+        {
+            var hitung = new Operasi();
+
+            if(index == 1)
+            {
+                listBox1.Items.Add(string.Format("Hasil Penjumlahan : {0} + {1} = {2}", a, b, hitung.Penjumlahan(a, b)));
+            }
+            else if(index == 2)
+            {
+                listBox1.Items.Add(string.Format("Hasil Pengurangan : {0} - {1} = {2}", a, b, hitung.Pengurangan(a, b)));
+            }
+            else if (index == 3)
+            {
+                listBox1.Items.Add(string.Format("Hasil Perkalian : {0} * {1} = {2}", a, b, hitung.Perkalian(a, b)));
+            }
+            else if(index == 4)
+            {
+                listBox1.Items.Add(string.Format("Hasil Pembagian : {0} / {1} = {2}", a, b, hitung.Pembagian(a, b)));
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HitungClick(object sender, EventArgs e)
+        {
+            EntryCalculator entryCalculator = new EntryCalculator();
+
+            entryCalculator.OnPerhitungan += FormHasilPerhitungan;
+            entryCalculator.ShowDialog();
+        }
+    }
+}
